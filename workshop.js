2a07f36fda616e0c3e42aca7fdc922ca14a32496
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-12 22:12:23 +0000

    jQuery textbox counter

---
+++ workshop.js
@@ -0,0 +1,17 @@
+$( "#the-textarea" ).keyup(function() {
+  // alert( "Handler for .keyup() called." );
+  console.log('Event: keyup');
+  var value = $(this).val();
+  $("span#character-counter").text(140-value.length);
+  var counter = value.length;
+  
+  if(counter<(140*0.8)) {
+      $( "span#character-counter" ).last().addClass( "green" );
+  }
+  else if(counter>=(140*0.8) && counter<=(140*0.9)) {
+      $( "span#character-counter" ).last().addClass( "yellow" );
+  }
+  else {
+      $( "span#character-counter" ).last().addClass( "red" );
+  }
+});
